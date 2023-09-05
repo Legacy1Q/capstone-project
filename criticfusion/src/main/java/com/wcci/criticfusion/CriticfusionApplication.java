f0a0d334f7bc3ace3250d71697154afd14ab47fc
emilycbroad <emily.leinenbach@yahoo.com>
2023-09-04 21:10:15 -0400

    Lets try like this

---
+++ criticfusion/src/main/java/com/wcci/criticfusion/CriticfusionApplication.java
@@ -0,0 +1,13 @@
+package com.wcci.criticfusion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CriticfusionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CriticfusionApplication.class, args);
+	}
+
+}
