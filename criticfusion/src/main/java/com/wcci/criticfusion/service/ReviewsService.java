e3c31e745e177fe316a91dcf5eec05fdcc994c2d
naiiaa <nikbbradshaw@gmail.com>
2023-09-08 20:11:00 -0400

    Nik's

---
+++ criticfusion/src/main/java/com/wcci/criticfusion/service/ReviewsService.java
@@ -0,0 +1,19 @@
+package com.wcci.criticfusion.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import com.wcci.criticfusion.entity.Reviews;
+import com.wcci.criticfusion.repository.ReviewsRepository;
+
+@Service
+public class ReviewsService{
+    @Autowired
+    private ReviewsRepository reviewsRepository;
+    public List<Reviews> getAllReviews() {
+    return reviewsRepository.findAll();
+    }
+}
