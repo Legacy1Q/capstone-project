cf11c3978cc49f7002313f2d843ab089a4c47152
emilycbroad <emily.leinenbach@yahoo.com>
2023-09-15 19:55:25 -0400

    updated tv put and delete

--- criticfusion/src/main/java/com/wcci/criticfusion/controller/TVController.java
+++ criticfusion/src/main/java/com/wcci/criticfusion/controller/TVController.java
@@ -3,8 +3,11 @@ package com.wcci.criticfusion.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -29,6 +32,16 @@ public class TVController {
 
         tvService.addTV(tv);
 
+    }
+
+    @PutMapping("/updateTV/{id}")
+    public TV updateTV(@PathVariable long id, @RequestBody TV tv) {
+        return this.tvService.updateTV(id, tv);
+    }
+
+    @DeleteMapping("/deleteTV/{id}")
+    public void removeTV(@PathVariable long id) {
+        this.tvService.deleteTV(id);
     }
 
 }
--- criticfusion/src/main/java/com/wcci/criticfusion/service/TVService.java
+++ criticfusion/src/main/java/com/wcci/criticfusion/service/TVService.java
@@ -4,7 +4,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.wcci.criticfusion.entity.TV;
 import com.wcci.criticfusion.repository.TVRepository;
@@ -20,9 +22,26 @@ public class TVService {
   return tvRepository.findAll();
   }
 
+  public TV findTVById(long id) {
+    return this.tvRepository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Id is not found."));
+  }
+
   public void addTV (TV tv){
     tvRepository.save(tv);
   }
+
+ public TV updateTV (long id, TV updatedDetails) {
+  TV exsistingTV = findTVById(id);
+  exsistingTV.setTitle(updatedDetails.getTitle() == null? exsistingTV.getTitle() : updatedDetails.getTitle());
+  exsistingTV.setDescription(updatedDetails.getDescription() == null? exsistingTV.getDescription() : updatedDetails.getDescription());
+  this.tvRepository.save(exsistingTV);
+  return exsistingTV;
+ }
+
+public void deleteTV(long id) {
+  this.tvRepository.deleteById(id);
+}
+
 
 
 
