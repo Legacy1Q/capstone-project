966f48f3abc074cdd9dedb6843dfb256b6473472
Legacy1Q <mdsims2@outlook.com>
2023-09-17 16:04:03 -0400

    added a carousel and rows for images

--- client-side/src/home/Home.jsx
+++ client-side/src/home/Home.jsx
@@ -1,9 +1,45 @@
+import { useEffect } from 'react';
 import './Home.css';
+import { Carousel, initTE, } from 'tw-elements'
 
 function Home() {
+  useEffect(() => {
+    initTE({ Carousel });
+  }, []);
+  
   return (
     <div className="home">
-      <h1>Home</h1>
+
+      {/* Carousel */}
+      <div className="home__header">
+        <div className="carousel-container">
+          {/* <Carousel /> */}
+        </div>
+      </div>
+
+      <div className="home__body">
+        <h2>Deals of the Day</h2>
+        <div className="image__container">
+          <img src="./images/Baldur's Gate 3.avif" alt="" />
+          <img src="./images/red dead 2.jpg" alt="" />
+        </div>
+
+        <h2>Movies</h2>
+        <div className="image__container">
+          <img src="" alt="" />
+        </div>
+
+        <h2>TV</h2>
+        <div className="image__container">
+          <img src="" alt="" /> 
+        </div>
+
+        <h2>Games</h2>
+        <div className="image__container">
+          <img src="" alt="" />  
+        </div>
+      </div>
+
     </div>
   )
 }
