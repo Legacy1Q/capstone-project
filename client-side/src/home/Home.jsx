0fe11c00663fa68d0726727d309f88e99f743338
Legacy1Q <mdsims2@outlook.com>
2023-09-21 23:33:04 -0400

    finished home page, a lot of testing :(

--- client-side/src/home/Home.jsx
+++ client-side/src/home/Home.jsx
@@ -1,44 +1,102 @@
+import { useState } from 'react';
 import './Home.css';
-import { useEffect, useState } from 'react';
+import Carousel from 'react-bootstrap/Carousel';
+
 
 
 
 function Home() {
-  const [activeIndex, setActiveIndex] = useState(0);
+  const [index, setIndex] = useState(0);
 
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setActiveIndex((prevIndex => (prevIndex) + 1) % 3);
-    }, 5000);
-    return () => clearInterval(interval);
-  }, []);
+  const handleSelect = (selectedIndex) => {
+      setIndex(selectedIndex);
+  };
 
-  
   return (
     <div className="home">
-
+      
       {/* Carousel */}
       <div className='home__carousel'>
-        <div id="carouselExample" className="carousel-slide">
-          <div className="carousel-inner">
-          {Array.from({ length: 3 }).map((_, index) => (
-              <div key={index} className={`carousel-item ${index === activeIndex ? 'active' : ''}`}>
-                <img src={`./images/Baldur's Gate 3.avif`} className="d-block w-100" alt={`Slide ${index + 1}`} />
-              </div>
-            ))}
+        <div className="carousel__container">
+          <div className="row">
+            <Carousel activeIndex={index} 
+                      onSelect={handleSelect}
+                      prevIcon={<img src="./images/left_arrow_icon.png" alt="Previous" />}
+                      nextIcon={<img src="./images/right_arrow_icon.png" alt="Next" />}>
+              <Carousel.Item>
+                <img src="./images/ahsoka.jpg" alt="" /> 
+              </Carousel.Item>
+              <Carousel.Item>
+                <img src="./images/red dead 2.jpg" alt="" />
+              </Carousel.Item>
+              <Carousel.Item>
+                <img src="./images/oppenheimer.jpeg" alt="" />
+              </Carousel.Item>
+              <Carousel.Item>
+                <img src="./images/barbie.jpg" alt="" />
+              </Carousel.Item>
+              <Carousel.Item>
+                <img src="./images/mortal kombat 1.jpg" alt="" />
+              </Carousel.Item>
+          </Carousel>
           </div>
-          <button className="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
-            <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span className="visually-hidden">Previous</span>
-          </button>
-          <button className="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
-            <span className="carousel-control-next-icon" aria-hidden="true"></span>
-            <span className="visually-hidden">Next</span>
-          </button>
         </div>
       </div>
 
+      {/* Body */}
+      <div className="home__body">
+        <div className="body__container">
+          <div className="row">
+            <div className="col-12 col-md-6">
+              <div className="body__container__1">
+                <div className="body__container__1__title">
+                  <h1>Movies</h1>
+                </div>
+                <img src="./images/barbie 1440.jpg" alt="" />
+                <img src="./images/oppenheimer 1440.jpg" alt="" />
+                <img src="./images/talk to me 1440.jpg" alt="" />
+                <img src="./images/tmnt.webp" alt="" />
+                <img src="./images/john wick 4.jpg" alt="" />
+                <img src="./images/fast x.jpg" alt="" />
+                <img src="./images/cocaine bear.jpeg" alt="" />
+              </div>
+            </div>
+            <div className="col-12 col-md-6">
+              <div className="body__container__2">
+                <div className="body__container__2__title">
+                  <h1>TV Shows</h1>
+                </div>
+                <img src="./images/ahsoka 1440.jpg" alt="" />
+                <img src="./images/one piece 1440.jpg" alt="" />
+                <img src="./images/stranger things.webp" alt="" />
+                <img src="./images/talk to me 1440.jpg" alt="" />
+                <img src="./images/the rookie 1440.jpg" alt="" />
+                <img src="./images/wednesday 1440.avif" alt="" />
+                <img src="./images/yellowstone 1440.jpg" alt="" />
+
+              </div>
+            </div>
+            <div className="col-12 col-md-6">
+              <div className="body__container__3">
+                <div className="body__container__3__title">
+                  <h1>Games</h1>
+                </div>
+                <img src="./images/Baldur's Gate 3.avif" alt="" />
+                <img src="./images/starfield.jpg" alt="" />
+                <img src="./images/nba-2k24.jpg" alt="" />
+                <img src="./images/fc24.webp" alt="" />
+                <img src="./images/warzone.jpg" alt="" />
+                <img src="./images/apex legends.jpg" alt="" />
+                <img src="./images/lies of p.jpg" alt="" />
+
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
+
+    
   )
 }
 
