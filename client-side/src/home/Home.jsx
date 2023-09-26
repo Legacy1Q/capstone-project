51e855cb526f7727969ccb549ce41bceca85c9a0
rheeze-dev <ggybzz2297@gmail.com>
2023-09-26 01:22:35 -0400

    Implemented api to auto fill movies images & title

--- client-side/src/home/Home.jsx
+++ client-side/src/home/Home.jsx
@@ -1,30 +1,53 @@
-import { useState } from 'react';
-import './Home.css';
-import Carousel from 'react-bootstrap/Carousel';
-
-
-
+import { useState, useEffect } from "react";
+import "./Home.css";
+import Carousel from "react-bootstrap/Carousel";
 
 function Home() {
   const [index, setIndex] = useState(0);
+  const [movie, setMovie] = useState([]);
+
+  const url = "https://imdb8.p.rapidapi.com/auto-complete?q=movies";
+  const options = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": "bf7d4a2827msh03ee1789278a56ep1fe3a5jsn0383e3886838",
+      "X-RapidAPI-Host": "imdb8.p.rapidapi.com",
+    },
+  };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(url, options);
+        const result = await response.json();
+        setMovie(result.d);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchData();
+  }, []);
 
   const handleSelect = (selectedIndex) => {
-      setIndex(selectedIndex);
+    setIndex(selectedIndex);
   };
 
   return (
     <div className="home">
-      
       {/* Carousel */}
-      <div className='home__carousel'>
+      <div className="home__carousel">
         <div className="carousel__container">
           <div className="row">
-            <Carousel activeIndex={index} 
-                      onSelect={handleSelect}
-                      prevIcon={<img src="./images/left_arrow_icon.png" alt="Previous" />}
-                      nextIcon={<img src="./images/right_arrow_icon.png" alt="Next" />}>
+            <Carousel
+              activeIndex={index}
+              onSelect={handleSelect}
+              prevIcon={
+                <img src="./images/left_arrow_icon.png" alt="Previous" />
+              }
+              nextIcon={<img src="./images/right_arrow_icon.png" alt="Next" />}
+            >
               <Carousel.Item>
-                <img src="./images/ahsoka.jpg" alt="" /> 
+                <img src="./images/ahsoka.jpg" alt="" />
               </Carousel.Item>
               <Carousel.Item>
                 <img src="./images/red dead 2.jpg" alt="" />
@@ -38,7 +61,7 @@ function Home() {
               <Carousel.Item>
                 <img src="./images/mortal kombat 1.jpg" alt="" />
               </Carousel.Item>
-          </Carousel>
+            </Carousel>
           </div>
         </div>
       </div>
@@ -52,13 +75,18 @@ function Home() {
                 <div className="body__container__1__title">
                   <h1>Movies</h1>
                 </div>
-                <img src="./images/barbie 1440.jpg" alt="" />
-                <img src="./images/oppenheimer 1440.jpg" alt="" />
-                <img src="./images/talk to me 1440.jpg" alt="" />
+                {movie.map((list, index) => (
+                  <img key={index} src={list.i.imageUrl} alt="" />
+                ))}
+                {movie.map((list, index) => (
+                  <p key={index}>{list.l}</p>
+                ))}
+                {/* <img src="./images/oppenheimer 1440.jpg" alt="" /> */}
+                {/* <img src="./images/talk to me 1440.jpg" alt="" />
                 <img src="./images/tmnt.webp" alt="" />
                 <img src="./images/john wick 4.jpg" alt="" />
                 <img src="./images/fast x.jpg" alt="" />
-                <img src="./images/cocaine bear.jpeg" alt="" />
+                <img src="./images/cocaine bear.jpeg" alt="" /> */}
               </div>
             </div>
             <div className="col-12 col-md-6">
@@ -73,7 +101,6 @@ function Home() {
                 <img src="./images/the rookie 1440.jpg" alt="" />
                 <img src="./images/wednesday 1440.avif" alt="" />
                 <img src="./images/yellowstone 1440.jpg" alt="" />
-
               </div>
             </div>
             <div className="col-12 col-md-6">
@@ -88,16 +115,13 @@ function Home() {
                 <img src="./images/warzone.jpg" alt="" />
                 <img src="./images/apex legends.jpg" alt="" />
                 <img src="./images/lies of p.jpg" alt="" />
-
               </div>
             </div>
           </div>
         </div>
       </div>
     </div>
-
-    
-  )
+  );
 }
 
 export default Home;
