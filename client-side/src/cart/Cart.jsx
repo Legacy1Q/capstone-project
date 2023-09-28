e61766c34331eb1e64a5d8143d683099658971de
Legacy1Q <mdsims2@outlook.com>
2023-09-28 15:38:07 -0400

    react-bootstap, have to redo styling

---
+++ client-side/src/cart/Cart.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import Button from 'react-bootstrap/Button';
+import './Cart.css';
+
+
+function Cart() {
+  return (
+    <div className='cart'>
+      <h1>Cart</h1>
+
+      <div className="cart__container">
+          <h2>Your Cart is Empty</h2>
+          <div className="cart__buttons">
+            <a href="/">
+                <Button variant="outline-secondary">See Whats Poppin</Button>{' '}
+            </a>
+            <a href="/login">
+                <Button variant="outline-light">Sign In</Button>{' '}
+            </a>
+          </div>
+      </div>
+    </div>
+  )
+}
+
+export default Cart;
