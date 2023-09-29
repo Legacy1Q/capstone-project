fa1dd4e29067355b5865f34b4f5f03e4b960882d
Legacy1Q <mdsims2@outlook.com>
2023-09-28 23:54:12 -0400

    something wrong with movies api

--- client-side/src/home/Home.jsx
+++ client-side/src/home/Home.jsx
@@ -36,36 +36,37 @@ function Home() {
 
   return (
     <div className="home">
+      
       {/* Carousel */}
       <div className="home__carousel">
         <div className="carousel__container">
-          <div className="row">
-            <Carousel activeIndex={index} onSelect={handleSelect}>
-              <Carousel.Item>
-                <a href="/review_tv">
-                  <img src="./images/ahsoka.jpg" alt="" />
-                </a>
-              </Carousel.Item>
-              <Carousel.Item>
+          <Carousel activeIndex={index} onSelect={handleSelect}>
+            <Carousel.Item>
+              <a href="/review_tv">
+                <img src="./images/ahsoka.jpg" alt="" />
+              </a>
+            </Carousel.Item>
+            <Carousel.Item>
+              <a href="/review_tv">
                 <img src="./images/red dead 2.jpg" alt="" />
-              </Carousel.Item>
-              <Carousel.Item>
+              </a>
+            </Carousel.Item>
+            <Carousel.Item>
+              <a href="/review_tv">
                 <img src="./images/oppenheimer.jpeg" alt="" />
-              </Carousel.Item>
-              <Carousel.Item>
+              </a>
+            </Carousel.Item>
+            <Carousel.Item>
+              <a href="/review_tv">
                 <img src="./images/barbie.jpg" alt="" />
-              </Carousel.Item>
-              <Carousel.Item>
+              </a>
+            </Carousel.Item>
+            <Carousel.Item>
+              <a href="/review_tv">
                 <img src="./images/mortal kombat 1.jpg" alt="" />
-              </Carousel.Item>
-            </Carousel>
-          </div>
-          <button className="carousel-control-prev" onClick={() => handleSelect(index - 1)} disabled={index === 0}>
-              <img src={'./images/left_arrow_icon.png'} alt="Previous" />
-          </button>
-          <button className="carousel-control-next" onClick={() => handleSelect(index + 1)} disabled={index === movie.length - 1}>
-              <img src={'./images/left_arrow_icon.png'} alt="Next" />
-          </button>
+              </a>
+            </Carousel.Item>
+          </Carousel>
         </div>
       </div>
 
@@ -73,50 +74,52 @@ function Home() {
       <div className="home__body">
         <div className="body__container">
           <div className="row">
-            <div className="col-12 col-md-6">
+            <div className="col-12">
               <div className="body__container__1">
                 <div className="body__container__1__title">
-                  <h1>Movies</h1>
+                    <h1>Movies</h1>
                 </div>
-                {movie.map((list, index) => (
+                {/* {movie.map((list, index) => (
                   <img key={index} src={list.i.imageUrl} alt="" />
                 ))}
                 {movie.map((list, index) => (
                   <p key={index}>{list.l}</p>
-                ))}
-                {/* <img src="./images/oppenheimer 1440.jpg" alt="" /> */}
-                {/* <img src="./images/talk to me 1440.jpg" alt="" />
+                ))} */}
+                <img src="./images/oppenheimer 1440.jpg" alt="" />
+                <img src="./images/talk_to_me.webp" alt="" />
                 <img src="./images/tmnt.webp" alt="" />
                 <img src="./images/john wick 4.jpg" alt="" />
                 <img src="./images/fast x.jpg" alt="" />
-                <img src="./images/cocaine bear.jpeg" alt="" /> */}
+                <img src="./images/cocaine bear.jpeg" alt="" /> 
               </div>
             </div>
-            <div className="col-12 col-md-6">
+          </div>
+          <div className="row">
+            <div className="col-12">
               <div className="body__container__2">
-                <div className="body__container__2__title">
-                  <h1>TV Shows</h1>
-                </div>
+                  <div className="body__container__2__title">
+                      <h1>TV</h1>
+                  </div>
                 <a href="/review_tv">
                   <img src="./images/ahsoka 1440.jpg" alt="" />
                 </a>
                 <img src="./images/one piece 1440.jpg" alt="" />
                 <img src="./images/stranger things.webp" alt="" />
-                <img src="./images/talk_to_me.webp" alt="" />
                 <img src="./images/the rookie 1440.jpg" alt="" />
                 <img src="./images/wednesday 1440.avif" alt="" />
                 <img src="./images/yellowstone 1440.jpg" alt="" />
-              </div>
+              </div>      
             </div>
-            <div className="col-12 col-md-6">
-              <div className="body__container__3">
-                <div className="body__container__3__title">
+          </div>
+          <div className="row">
+            <div className="col-12">
+              <div className="body__container__2">
+                <div className="body__container__2__title">
                   <h1>Games</h1>
                 </div>
                 <img src="./images/Baldur's Gate 3.avif" alt="" />
                 <img src="./images/starfield.jpg" alt="" />
                 <img src="./images/nba-2k24.jpg" alt="" />
-                <img src="./images/fc24.webp" alt="" />
                 <img src="./images/warzone.jpg" alt="" />
                 <img src="./images/apex legends.jpg" alt="" />
                 <img src="./images/lies of p.jpg" alt="" />
@@ -125,6 +128,7 @@ function Home() {
           </div>
         </div>
       </div>
+
     </div>
   );
 }
