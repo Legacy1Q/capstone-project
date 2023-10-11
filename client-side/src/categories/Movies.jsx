975cf25b06c1f9825670c4253a14b2626dbeca15
rheeze-dev <ggybzz2297@gmail.com>
2023-10-10 21:38:33 -0400

    Worked on the modal design when image is clicked

--- client-side/src/categories/Movies.jsx
+++ client-side/src/categories/Movies.jsx
@@ -1,8 +1,24 @@
 import { useEffect, useState } from "react";
 import "./Movies.css";
+import Modal from "react-modal";
+
+Modal.setAppElement("#root");
 
 function Movies() {
   const [data, setData] = useState([]);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [editedDataId, setEditedDataId] = useState(null);
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
+  function imageClickHandler(id) {
+    setEditedDataId(id);
+    setIsModalOpen(true);
+    console.log(editedDataId);
+  }
+
   useEffect(() => {
     const options = {
       method: "GET",
@@ -26,6 +42,75 @@ function Movies() {
 
   return (
     <div className="movies">
+      <Modal
+        isOpen={isModalOpen}
+        onRequestClose={closeModal}
+        style={{
+          content: {
+            width: "70%",
+            margin: "auto",
+            height: "90%",
+            overflow: "hidden",
+          },
+        }}
+      >
+        <h2 className="title-text">
+          {data.find((item) => item.id === editedDataId)?.title ||
+            "Item not found"}
+        </h2>
+        <hr />
+        <div style={{ display: "flex", maxHeight: "100%" }}>
+          <div style={{ flex: 1 }}>
+            <img
+              src={`https://image.tmdb.org/t/p/w300/${
+                data.find((item) => item.id === editedDataId)?.poster_path
+              }`}
+              alt=""
+              style={{ width: "100%", height: "100%" }}
+            />
+          </div>
+          <div
+            style={{
+              flex: 1,
+              padding: "20px",
+              display: "flex",
+              flexDirection: "column",
+            }}
+            className="data-div"
+          >
+            <div style={{ flex: 2, marginBottom: "20px" }}>
+              <h3>Overview:</h3>
+              <h3>
+                {/* Overview:{" "} */}
+                {data.find((item) => item.id === editedDataId)?.overview}
+              </h3>
+            </div>
+            <hr />
+            <div style={{ flex: 2 }}>
+              <p>
+                Media type:{" "}
+                {data.find((item) => item.id === editedDataId)?.media_type}
+              </p>
+              <p>
+                Popularity:{" "}
+                {data.find((item) => item.id === editedDataId)?.popularity}
+              </p>
+              <p>
+                Release date:{" "}
+                {data.find((item) => item.id === editedDataId)?.release_date}
+              </p>
+              <p>
+                Vote average:{" "}
+                {data.find((item) => item.id === editedDataId)?.vote_average}
+              </p>
+              <p>
+                Vote count:{" "}
+                {data.find((item) => item.id === editedDataId)?.vote_count}
+              </p>
+            </div>
+          </div>
+        </div>
+      </Modal>
       <h1 className="movies__title">Movies</h1>
       <div className="movies__body">
         <div className="movies__container">
@@ -37,14 +122,17 @@ function Movies() {
                     <img
                       src={`https://image.tmdb.org/t/p/w300/${item.poster_path}`}
                       alt={item.title}
+                      onClick={() => {
+                        imageClickHandler(item.id);
+                      }}
                     />
                     <p className="movie-title">{item.title}</p>
-                    <p>Overview: {item.overview}</p>
+                    {/* <p>Overview: {item.overview}</p>
                     <p>Media type: {item.media_type}</p>
                     <p>Popularity: {item.popularity}</p>
                     <p>Release date: {item.release_date}</p>
                     <p>Vote average: {item.vote_average}</p>
-                    <p>Vote count: {item.vote_count}</p>
+                    <p>Vote count: {item.vote_count}</p> */}
                   </div>
                 </div>
               </div>
