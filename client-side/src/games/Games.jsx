dbe21e5e6ecff2b5ceedc38114cee8a1caa7ab94
rheeze-dev <ggybzz2297@gmail.com>
2023-10-12 19:24:15 -0400

    Completed code for the landing page of games tab

--- client-side/src/games/Games.jsx
+++ client-side/src/games/Games.jsx
@@ -1,62 +1,240 @@
+import { useState, useEffect } from "react";
+import Modal from "react-modal";
+import Rating from "react-rating-stars-component";
 import "./Games.css";
 
+Modal.setAppElement("#root");
+
 function Games() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [review, setReview] = useState("");
+  const [rating, setRating] = useState(0);
+  const [games, setGames] = useState([]);
+  const [editedDataId, setEditedDataId] = useState(null);
+  const [gameReviews, setGameReviews] = useState([]);
+  const [modalDisplay, setModalDisplay] = useState(false);
+  const uniqueGameIds = [
+    ...new Set(gameReviews.map((review) => review.games.id)),
+  ];
+
+  useEffect(() => {
+    fetch("http://localhost:8080/games")
+      .then((response) => response.json())
+      .then((data) => setGames(data))
+      .catch((error) => console.error("Error fetching data:", error));
+    fetch("http://localhost:8080/gameReviews")
+      .then((response) => response.json())
+      .then((data) => setGameReviews(data))
+      .catch((error) => console.error("Error fetching data:", error));
+  }, []);
+
+  function submitHandler() {
+    if (review == "" || rating == 0) {
+      return alert("Please enter a review or rating!");
+    }
+    gameReview();
+
+    alert("Added review successfully!");
+    setReview("");
+    setModalDisplay(false);
+  }
+
+  function calculateAverageRatingForId(reviews, category, id) {
+    let filteredReviews;
+    if (category === "movie") {
+      filteredReviews = reviews.filter(
+        (review) => review.movie && review.movie.id === id
+      );
+    } else if (category === "tv") {
+      filteredReviews = reviews.filter(
+        (review) => review.tv && review.tv.id === id
+      );
+    } else {
+      filteredReviews = reviews.filter(
+        (review) => review.games && review.games.id === id
+      );
+    }
+
+    if (filteredReviews.length === 0) {
+      return 0;
+    }
+
+    const totalRating = filteredReviews.reduce(
+      (sum, review) => sum + review.rating,
+      0
+    );
+    const averageRating = totalRating / filteredReviews.length;
+    return averageRating.toFixed(1);
+  }
+
+  const averageGameRating = uniqueGameIds.map((id) =>
+    calculateAverageRatingForId(gameReviews, "game", id)
+  );
+
+  const reviewHandler = (object) => {
+    openModal();
+    setEditedDataId(object.id);
+  };
+
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
+  async function gameReview() {
+    const response = await fetch("http://localhost:8080/addGameReview", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-type": "application/json; charset=UTF-8",
+      },
+      body: JSON.stringify({
+        review: review,
+        rating: rating,
+        games: {
+          id: editedDataId,
+        },
+      }),
+    });
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    const newData = await response.json();
+    setGameReviews((prevGameReviews) => [...prevGameReviews, newData]);
+  }
+
   return (
     <div className="games">
+      <Modal
+        isOpen={isModalOpen}
+        onRequestClose={closeModal}
+        style={{
+          content: {
+            width: "60%",
+            margin: "auto",
+            height: "60%",
+          },
+        }}
+      >
+        <h2 className="modal-title">
+          {games.find((game) => game.id === editedDataId)?.title}
+          <button
+            className="button modal-button float-start inline"
+            onClick={() => {
+              setModalDisplay(!modalDisplay);
+            }}
+          >
+            {modalDisplay ? "View reviews" : "Add reviews"}
+          </button>
+          <button
+            type="button"
+            className="btn-close float-end"
+            onClick={() => {
+              setIsModalOpen(false);
+              setModalDisplay(false);
+              setReview("");
+              setRating(0);
+            }}
+            aria-label="Close"
+          ></button>
+        </h2>
+        <div className={!modalDisplay ? "hide" : "form-floating mb-3"}>
+          <input
+            type="text"
+            className="form-control form-control-md"
+            placeholder="Review"
+            id="floatingInput"
+            value={modalDisplay ? review : ""}
+            onInput={(e) => setReview(e.target.value)}
+          />
+          <label htmlFor="floatingInput">Review</label>
+        </div>
+        <div className={!modalDisplay ? "hide" : "rating"}>
+          <Rating
+            count={5}
+            onChange={(newRating) => {
+              setRating(newRating);
+            }}
+            size={100}
+            color="gray"
+            activeColor={"#FFD700"}
+          />
+        </div>
+        <h2 className={!modalDisplay ? "hide" : "rating-text"}>Rating</h2>
+        <table
+          className={
+            !modalDisplay
+              ? "table table-striped table-hover modal-table"
+              : "hide"
+          }
+        >
+          <thead>
+            <tr>
+              <th scope="col" className="col-10">
+                Review
+              </th>
+              <th scope="col" className="col-2">
+                Rating
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {gameReviews
+              .filter((game) => game.games.id === editedDataId)
+              .map((game) => (
+                <tr key={game.id}>
+                  <td>{game.review}</td>
+                  <td>{game.rating}</td>
+                </tr>
+              ))}
+          </tbody>
+        </table>
+
+        <button
+          className={
+            modalDisplay == false ? "hide" : "form-button button1 button-submit"
+          }
+          onClick={submitHandler}
+        >
+          Submit
+        </button>
+        <button
+          className={
+            modalDisplay == false ? "hide" : "form-button button1 button-cancel"
+          }
+          onClick={() => {
+            setModalDisplay(false);
+            closeModal();
+            setReview("");
+            setRating(0);
+          }}
+        >
+          Cancel
+        </button>
+      </Modal>
       <h1>Games</h1>
       <div className="games__body">
         <div className="games__container">
           <div className="row">
-            <div className="col-12 col-md-6">
-              <div className="games__container__1">
-                <img src="./Downloads/images.jpeg" alt="" />
-                <img src="./images/barbie 1440.jpg" alt="" />
-                <img src="./images/cocaine bear.jpeg" alt="" />
-                <img src="./images/fast x.jpg" alt="" />
-                <img src="./images/john wick 4.jpg" alt="" />
-                <img src="./images/talk_to_me.webp" alt="" />
+            {games.map((item) => (
+              <div className="col-6 col-md-4" key={item.id}>
+                <div className="game-card">
+                  <div className="game-poster-container">
+                    <img
+                      src="./images/starfield.jpg"
+                      alt=""
+                      className="displayed-img"
+                      onClick={() => {
+                        reviewHandler(item.id);
+                      }}
+                    />
+                  </div>
+                </div>
               </div>
-            </div>
-            <div className="col-12 col-md-6">
-              <div className="games__container__1">
-                <img src="./images/creed_iii.webp" alt="" />
-                <img src="./images/black_adam.webp" alt="" />
-                <img src="./images/top_maverick.webp" alt="" />
-                <img src="./images/megan.webp" alt="" />
-                <img src="./images/black_phone.webp" alt="" />
-                <img src="./images/super_mario.webp" alt="" />
-              </div>
-            </div>
-            <div className="col-12 col-md-6">
-              <div className="games__container__1">
-                <img src="./images/blue_beetle.png" alt="" />
-                <img src="./images/the_flash.webp" alt="" />
-                <img src="./images/come_out_fighing.webp" alt="" />
-                <img src="./images/demeter.webp" alt="" />
-                <img src="./images/the_requin.webp" alt="" />
-                <img src="./images/tmnt.webp" alt="" />
-              </div>
-            </div>
-            <div className="col-12 col-md-6">
-              <div className="games__container__1">
-                <img src="./images/retribution.webp" alt="" />
-                <img src="./images/tmnt.webp" alt="" />
-                <img src="./images/coweb.webp" alt="" />
-                <img src="./images/kandahar.webp" alt="" />
-                <img src="./images/equalizer_2.webp" alt="" />
-                <img src="./images/good_boys.webp" alt="" />
-              </div>
-            </div>
-            <div className="col-12 col-md-6">
-              <div className="games__container__1">
-                <img src="./images/puss_in_boots.webp" alt="" />
-                <img src="./images/crazy_rich_asians.webp" alt="" />
-                <img src="./images/the_whale.jpg" alt="" />
-                <img src="./images/spiral.webp" alt="" />
-                <img src="./images/retribution.webp" alt="" />
-                <img src="./images/the_lion_king.webp" alt="" />
-              </div>
-            </div>
+            ))}
           </div>
         </div>
       </div>
