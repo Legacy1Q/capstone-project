b5db16fee1d031fbaafbe6871f303a7853183b47
rheeze-dev <ggybzz2297@gmail.com>
2023-10-12 16:12:48 -0400

    Completed all functionalities of search feature.

--- client-side/src/home/Nav.jsx
+++ client-side/src/home/Nav.jsx
@@ -4,6 +4,7 @@ import "./Nav.css";
 import SearchIcon from "@mui/icons-material/Search";
 import ShoppingBagIcon from "@mui/icons-material/ShoppingBag";
 import CircleIcon from "@mui/icons-material/Circle";
+import Youtube from "react-youtube";
 import { useContext } from "react";
 import { MyContext } from "../MyContext";
 import Modal from "react-modal";
@@ -12,8 +13,12 @@ function Nav() {
   const { adminEmail, updateAdminEmail, cart } = useContext(MyContext);
   const [searchQuery, setSearchQuery] = useState("");
   const [searchedData, setSearchData] = useState([]);
+  const [editedDataId, setEditedDataId] = useState(null);
+  const [movieTrailer, setMovieTrailer] = useState(null);
   const [isSearchedDataModalOpen, setIsSearchedDataModalOpen] = useState(false);
+  const [isClickedDataModalOpen, setIsClickedDataModalOpen] = useState(false);
   const [isTrailerModalOpen, setIsTrailerModalOpen] = useState(false);
+  const apiKey = "e4ea514e7e06ce24e90f01250baf128d"; // Replace with your actual API key
   // const navigate = useNavigate();
 
   // const handleSearch = async () => {
@@ -25,8 +30,9 @@ function Nav() {
   // };
 
   const handleKeyPress = (e) => {
-    if (e.key === "enter") {
+    if (e.key === "Enter") {
       fetchSearchedData();
+      setIsSearchedDataModalOpen(true);
     }
   };
 
@@ -37,7 +43,21 @@ function Nav() {
   const closeModal = () => {
     setIsSearchedDataModalOpen(false);
     setIsTrailerModalOpen(false);
+    setIsClickedDataModalOpen(false);
   };
+
+  const trailerClickHandler = () => {
+    setIsTrailerModalOpen(true);
+    setIsSearchedDataModalOpen(false);
+    setIsClickedDataModalOpen(false);
+  };
+
+  function imageClickHandler(id) {
+    setEditedDataId(id);
+    setIsClickedDataModalOpen(true);
+    fetchMovieTrailer(id);
+    setIsSearchedDataModalOpen(false);
+  }
 
   const fetchSearchedData = () => {
     const options = {
@@ -58,71 +78,20 @@ function Nav() {
       .catch((err) => console.error(err));
   };
 
+  const fetchMovieTrailer = (id) => {
+    fetch(
+      `https://api.themoviedb.org/3/movie/${id}?api_key=${apiKey}&append_to_response=videos`
+    )
+      .then((response) => response.json())
+      .then((response) => {
+        setMovieTrailer(response.videos.results);
+      })
+      .catch((err) => console.error(err));
+  };
+
   return (
     <div className="nav">
       {/* Logo */}
-      {/* <Modal
-        isOpen={isSearchedDataModalOpen}
-        onRequestClose={closeModal}
-        style={{
-          content: {
-            width: "70%",
-            margin: "auto",
-            height: "90%",
-            overflow: "hidden",
-          },
-        }}
-      >
-        {searchedData.map((item) => (
-          <div key={item.id}>
-            <div className="modal-header">
-              <h2>{item.title || "Item not found"}</h2>
-              <button
-                type="button"
-                className="btn-close close-btn"
-                onClick={closeModal}
-                aria-label="Close"
-              ></button>
-            </div>
-            <hr />
-            <div style={{ display: "flex", maxHeight: "100%" }}>
-              <div style={{ flex: 1 }}>
-                <img
-                  src={`https://image.tmdb.org/t/p/w300/${item.poster_path}`}
-                  alt=""
-                  style={{ width: "100%", height: "90%" }}
-                  className="modal-img"
-                />
-              </div>
-              <div
-                style={{
-                  flex: 1,
-                  padding: "20px",
-                  display: "flex",
-                  flexDirection: "column",
-                }}
-                className="data-div"
-              >
-                <div style={{ flex: 2, marginBottom: "20px" }}>
-                  <h2 className="overview-text">Overview</h2>
-                  <h3>{item.overview}</h3>
-                </div>
-                <hr />
-                <div style={{ flex: 2 }}>
-                  <p>Release date: {item.release_date}</p>
-                  <p>Popularity: {item.popularity}</p>
-                  <p>Vote count: {item.vote_count}</p>
-                  <p>Vote average: {item.vote_average}</p>
-                  <p>
-                    Click button to view trailer:{" "}
-                    <button onClick={trailerClickHandler}>Trailer</button>
-                  </p>
-                </div>
-              </div>
-            </div>
-          </div>
-        ))}
-      </Modal> */}
       <Modal
         isOpen={isSearchedDataModalOpen}
         onRequestClose={closeModal}
@@ -144,7 +113,6 @@ function Nav() {
             aria-label="Close"
           ></button>
         </div>
-        {/* <hr className="sticky-hr" /> */}
 
         <div className="modal-images row">
           {searchedData.map(
@@ -155,11 +123,147 @@ function Nav() {
                     src={`https://image.tmdb.org/t/p/w300/${item.poster_path}`}
                     alt={item.title}
                     className="modal-img img-fluid"
+                    onClick={() => {
+                      imageClickHandler(item.id);
+                    }}
                   />
                 </div>
               )
           )}
         </div>
+      </Modal>
+      <Modal
+        isOpen={isClickedDataModalOpen}
+        onRequestClose={closeModal}
+        style={{
+          content: {
+            width: "70%",
+            margin: "auto",
+            height: "90%",
+            overflow: "hidden",
+            // zIndex: 10,
+          },
+        }}
+      >
+        <div className="modal-header">
+          <h2>
+            {searchedData.find((item) => item.id === editedDataId)?.title ||
+              "Item not found"}
+          </h2>
+          <button
+            type="button"
+            className="btn-close close-btn"
+            onClick={closeModal}
+            aria-label="Close"
+          ></button>
+        </div>
+        <hr />
+        <div style={{ display: "flex", maxHeight: "100%" }}>
+          <div style={{ flex: 1 }}>
+            <img
+              src={`https://image.tmdb.org/t/p/w300/${
+                searchedData.find((item) => item.id === editedDataId)
+                  ?.poster_path
+              }`}
+              alt=""
+              style={{ width: "100%", height: "90%" }}
+              className="modal-img"
+            />
+          </div>
+          <div
+            style={{
+              flex: 1,
+              padding: "20px",
+              display: "flex",
+              flexDirection: "column",
+            }}
+            className="data-div"
+          >
+            <div style={{ flex: 2, marginBottom: "20px" }}>
+              {/* <h2 className="overview-text">Overview</h2> */}
+              <h3>
+                {
+                  searchedData.find((item) => item.id === editedDataId)
+                    ?.overview
+                }
+              </h3>
+            </div>
+            <hr />
+            <div style={{ flex: 2 }}>
+              <p>
+                Release date:{" "}
+                {
+                  searchedData.find((item) => item.id === editedDataId)
+                    ?.release_date
+                }
+              </p>
+              <p>
+                Popularity:{" "}
+                {
+                  searchedData.find((item) => item.id === editedDataId)
+                    ?.popularity
+                }
+              </p>
+              <p>
+                Vote count:{" "}
+                {
+                  searchedData.find((item) => item.id === editedDataId)
+                    ?.vote_count
+                }
+              </p>
+              <p>
+                Vote average:{" "}
+                {
+                  searchedData.find((item) => item.id === editedDataId)
+                    ?.vote_average
+                }
+              </p>
+              <p>
+                Click button to view trailer:{" "}
+                <button onClick={trailerClickHandler}>Trailer</button>
+              </p>
+            </div>
+          </div>
+        </div>
+      </Modal>
+      <Modal
+        isOpen={isTrailerModalOpen}
+        onRequestClose={closeModal}
+        style={{
+          content: {
+            display: "flex", // Set display to flex
+            alignItems: "center", // Center vertically
+            justifyContent: "center",
+            width: "50%",
+            margin: "auto",
+            height: "60%",
+            overflow: "hidden",
+            // position: "relative",
+          },
+        }}
+      >
+        <button
+          type="button"
+          className="btn-close close-btn"
+          onClick={closeModal}
+          aria-label="Close"
+          style={{
+            position: "absolute", // Set position to absolute
+            top: "10px", // Adjust top to position it from the top
+            right: "10px", // Adjust right to position it from the right
+          }}
+        ></button>
+        <Youtube
+          videoId={
+            movieTrailer &&
+            (movieTrailer.length > 0
+              ? (
+                  movieTrailer.find((vid) => vid.name === "Official Trailer") ||
+                  movieTrailer.find((vid) => vid.name.includes("Trailer"))
+                )?.key
+              : "Sorry, no trailer was found.")
+          }
+        />
       </Modal>
 
       <div className="nav__header">
