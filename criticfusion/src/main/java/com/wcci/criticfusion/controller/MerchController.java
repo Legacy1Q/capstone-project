386812620d9735e9a56c077c9f000fe84f024b14
rheeze-dev <ggybzz2297@gmail.com>
2023-10-14 23:08:26 -0400

    Added backend for Merch. Entity,Service,Controller

---
+++ criticfusion/src/main/java/com/wcci/criticfusion/controller/MerchController.java
@@ -0,0 +1,51 @@
+package com.wcci.criticfusion.controller;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Value;
+import com.wcci.criticfusion.entity.Merch;
+import com.wcci.criticfusion.service.MerchService;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@RestController
+public class MerchController {
+
+  @Value("${image.upload.directory}")
+    private String imageUploadDirectory;
+
+  @Autowired
+
+  MerchService merchService;
+  
+  @GetMapping("/merch")
+  public List<Merch> getAllMerch() {
+    return merchService.findAllMerch();
+  }
+
+  @GetMapping("/merch/{id}")
+  public Merch getMerchById(@PathVariable long id) {
+    return merchService.findMerchById(id);
+  }
+
+  @PostMapping("/addMerch")
+  public Merch addMerch (@RequestBody Merch merch) {
+    return merchService.addMerch(merch);
+  }
+
+  @PutMapping("/updateMerch/{id}")
+  public Merch updateMerch(@PathVariable long id, @RequestBody Merch merch) {
+    return this.merchService.updateMerch(id, merch);
+  }
+
+  @DeleteMapping("/deleteMerch/{id}")
+  public void deleteMerch(@PathVariable long id) {
+    this.merchService.deleteMerch(id);
+  }
+
+}
---
+++ criticfusion/src/main/java/com/wcci/criticfusion/service/MerchService.java
@@ -0,0 +1,48 @@
+package com.wcci.criticfusion.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import com.wcci.criticfusion.entity.Merch;
+import com.wcci.criticfusion.repository.MerchRepository;
+
+@Service
+
+public class MerchService {
+
+  @Autowired
+  private MerchRepository merchRepository;
+
+  public List<Merch> findAllMerch() {
+  return merchRepository.findAll();
+  }
+
+  public Merch findMerchById(long id) {
+    return this.merchRepository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Id is not found."));
+  }
+
+  public Merch addMerch (Merch merch){
+    return merchRepository.save(merch);
+  }
+
+public Merch updateMerch(long id, Merch updatedMerch) {
+    Merch exsistingMerch = findMerchById(id);
+    exsistingMerch.setName(updatedMerch.getName() == null ? exsistingMerch.getName() : updatedMerch.getName());
+    exsistingMerch.setName(updatedMerch.getName() == null ? exsistingMerch.getName() : updatedMerch.getName());
+    exsistingMerch.setPrice(updatedMerch.getPrice());
+    exsistingMerch.setImage(updatedMerch.getImage());
+    exsistingMerch.setIsAddedToCart(updatedMerch.getIsAddedToCart());
+    exsistingMerch.setQuantity(updatedMerch.getQuantity());
+    exsistingMerch.setIsFavorite(updatedMerch.getIsFavorite());
+    this.merchRepository.save(exsistingMerch);
+    return exsistingMerch;
+}
+
+public void deleteMerch(long id) {
+  this.merchRepository.deleteById(id);
+}
+}
